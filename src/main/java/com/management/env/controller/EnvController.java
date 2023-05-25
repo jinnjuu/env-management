1dd1b9e9c42a51f2b85558ac321a18c284fd1c0d
jinnjuu <kjinnjuu@gmail.com>
2023-05-26 00:57:31 +0900

    initial commit

---
+++ src/main/java/com/management/env/controller/EnvController.java
@@ -0,0 +1,14 @@
+package com.management.env.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EnvController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/management/env/EnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
